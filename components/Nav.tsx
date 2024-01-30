eb2f1add5c34c32c9586f254b6c0bcaebd08f451
Faizan Syed <f35syed@uwaterloo.ca>
2024-01-29 20:02:53 -0500

    Add dynamic export to Nav component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ import { getProviders, signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-
+export const dynamic = "force-dynamic";
 const Nav = () => {
   const { data: session } = useSession();
 
