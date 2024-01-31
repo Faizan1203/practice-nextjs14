ad2bc411d4d476d5c1705822cfd0ac4c639724a6
Faizan Syed <f35syed@uwaterloo.ca>
2024-01-31 11:54:51 -0500

    Add new dependencies and update Nav component and added favicon

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,8 +2,11 @@
 import { getProviders, signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import GoogleIcon from "@mui/icons-material/Google";
+import GitHubIcon from "@mui/icons-material/GitHub";
 const Nav = () => {
   const { data: session } = useSession();
 
@@ -55,17 +58,57 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={(provider as any).name}
-                  onClick={() => signIn((provider as any).id)}
-                  className="black_btn"
-                >
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className={"black_btn"}>
                   Sign In
-                </button>
-              ))}
+                  <ChevronDownIcon
+                    className="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    {providers &&
+                      Object.values(providers).map((provider) => (
+                        <Menu.Item key={(provider as any).name}>
+                          {({ active }) => (
+                            <div>
+                              <button
+                                className={`${
+                                  active
+                                    ? "bg-black text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                {(provider as any).name === "Google" && (
+                                  <GoogleIcon />
+                                )}
+                                {(provider as any).name === "GitHub" && (
+                                  <GitHubIcon />
+                                )}
+                                <p className="px-2">
+                                  Sign in with {(provider as any).name}
+                                </p>
+                              </button>
+                            </div>
+                          )}
+                        </Menu.Item>
+                      ))}
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </>
         )}
       </div>
